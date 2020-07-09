e93181c9d55d0dadeeb44fe79dc96ac59b801166
jorgeleiba <jorgeleiba@gmail.com>
2020-07-09 15:00:22 -0300

    nuevo mensaje

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,6 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("buenas noches test");
+	alert("prueba numero uno");
 }
 
